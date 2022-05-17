b5626a5c549c2356b121635d6938a8dd84c9e532
hanada31 <hanada31@163.com>
2022-05-17 16:08:33 +0800

    fix all file.separator

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -271,7 +271,7 @@ public class MainClass {
 		MyConfig.getInstance().setCrashInfoFilePath(mCmd.getOptionValue("crashPath","Files"+File.separator+"crashInfo.json"));
 
 		String sdkVersion = mCmd.getOptionValue("SDKVersion","9.0");
-		String androidFolder = "Files"+File.separator+"android"+File.separator+sdkVersion+File.separator;
+		String androidFolder = "Files"+File.separator+"android"+sdkVersion+File.separator;
 		MyConfig.getInstance().setExceptionFilePath(mCmd.getOptionValue("exceptionPath",androidFolder+"exceptionInfo"+File.separator));
 		MyConfig.getInstance().setAndroidCGFilePath(mCmd.getOptionValue("androidCGPath",androidFolder+"CallGraphInfo"+File.separator+"cg.txt"));
 		MyConfig.getInstance().setPermissionFilePath(mCmd.getOptionValue("permissionPath",androidFolder+"Permission"+File.separator+"permission.txt"));
