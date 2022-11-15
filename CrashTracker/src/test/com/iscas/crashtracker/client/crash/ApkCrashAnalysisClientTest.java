f480f97165dee6152013e2fb1493c6346c0e997b
hanada31 <hanada31@163.com>
2022-11-15 13:43:20 +0800

    modify the call graph analysis to CHA for testing

--- CrashTracker/src/test/com/iscas/crashtracker/client/crash/ApkCrashAnalysisClientTest.java
+++ CrashTracker/src/test/com/iscas/crashtracker/client/crash/ApkCrashAnalysisClientTest.java
@@ -37,7 +37,7 @@ public class ApkCrashAnalysisClientTest {
 //        name = "org.kontalk-773.apk";
         name = "cgeo.geocaching-1503.apk";
         name = "com.Android56-123.apk";
-        name = "com.influenster-223.apk";
+//        name = "com.influenster-223.apk";
 //        name = "com.source.coach-282.apk";
 
 
@@ -57,6 +57,9 @@ public class ApkCrashAnalysisClientTest {
 
         MyConfig.getInstance().setCrashInfoFilePath("..\\Files\\crashInfo.json");
         MyConfig.getInstance().setExceptionFolderPath("..\\Files\\");
+        MyConfig.getInstance().setCallgraphAlgorithm("SPARK");
+//        MyConfig.getInstance().setCallgraphAlgorithm("CHA");
+
 
     }
 
