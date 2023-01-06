d0ed48c8f0320754e7c531ec6505271eb8581510
hanada31 <hanada31@163.com>
2023-01-06 23:31:55 +0800

    add zip ETSs

---
+++ scripts/crashLocation.sh
@@ -0,0 +1,58 @@
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0901-ExtendCGOnly  "no" "ExtendCGOnly" 
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0901-ExtendCGOnly  "no" "ExtendCGOnly" 
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0901-ExtendCGOnly  "no" "ExtendCGOnly" 
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0901-version8 "8.0"  "no" 
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0901-version8 "8.0"  "no" 
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0901-version8 "8.0"  "no" 
+
+
+
+
+python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-1104-baseline "no" "no" 
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-1104-baseline "no" "no"  
+python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-1104-baseline "no" "no"  
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-ExtendCGOnly  "no" "ExtendCGOnly" 
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-ExtendCGOnly  "no" "ExtendCGOnly" 
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-ExtendCGOnly  "no" "ExtendCGOnly" 
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-version2 "2.3"  "no" 
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-version2 "2.3"  "no"  
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-version2 "2.3"  "no" 
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-version12 "12.0"  "no" 
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-version12 "12.0"  "no"  
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-version12 "12.0"  "no" 
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-NOParaChainANDDataTrace  "no" "NOParaChainANDDataTrace"  
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-NOParaChainANDDataTrace  "no" "NOParaChainANDDataTrace"   
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-NOParaChainANDDataTrace  "no" "NOParaChainANDDataTrace"   
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-NoSourceType "no" "NoSourceType"  
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-NoSourceType "no" "NoSourceType"  
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-NoSourceType  "no" "NoSourceType"  
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-NoKeyAPI "no" "NoKeyAPI"  
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-NoKeyAPI "no" "NoKeyAPI"  
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-NoKeyAPI "no" "NoKeyAPI"  
+
+# python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-NoCallFilter "no" "NoCallFilter"  
+# python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-NoCallFilter "no" "NoCallFilter"  
+# python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-NoCallFilter "no" "NoCallFilter"  
+
+
+
+# # python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-NoParaChain "no" "NoParaChain"  
+# # python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-NoParaChain "no"  "NoParaChain"  
+# # python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-NoParaChain "no" "NoParaChain"  
+
+# # python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-NoAppDataTrace "no" "NoAppDataTrace"  
+# # python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-NoAppDataTrace "no" "NoAppDataTrace"  
+# # python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-NoAppDataTrace "no" "NoAppDataTrace"  
+
+# # python scripts/runCrashTracker-Apk.py  ../../apk/FanDataICSE2018-before/ Data-0902-version4  "4.4"  "no" 
+# # python scripts/runCrashTracker-Apk.py  ../../apk/Empirical500/ Data-0902-version4  "4.4"  "no"  
+# # python scripts/runCrashTracker-Jar.py  ../../apk/android-sdk-project/ Data-0902-version4  "4.4"  "no" 
+
---
+++ scripts/summaryConstruction.sh
@@ -0,0 +1,34 @@
+
+echo python scripts/runCrashTracker-framework.py 2.3
+python scripts/runCrashTracker-framework.py 2.3
+
+echo  python scripts/runCrashTracker-framework.py 4.4
+python scripts/runCrashTracker-framework.py 4.4
+
+echo python scripts/runCrashTracker-framework.py 5.0
+python scripts/runCrashTracker-framework.py 5.0
+
+echo python scripts/runCrashTracker-framework.py 6.0
+python scripts/runCrashTracker-framework.py 6.0
+
+echo python scripts/runCrashTracker-framework.py 7.0
+python scripts/runCrashTracker-framework.py 7.0
+
+echo python scripts/runCrashTracker-framework.py 8.0
+python scripts/runCrashTracker-framework.py 8.0
+
+echo python scripts/runCrashTracker-framework.py 9.0
+python scripts/runCrashTracker-framework.py 9.0
+
+echo python scripts/runCrashTracker-framework.py 10.0
+python scripts/runCrashTracker-framework.py 10.0
+
+echo python scripts/runCrashTracker-framework.py 11.0
+python scripts/runCrashTracker-framework.py 11.0
+
+echo python scripts/runCrashTracker-framework.py 12.0
+python scripts/runCrashTracker-framework.py 12.0
+
+tar -czvf Files/newAllCond-android.tar.gz Files/android*
+
+
